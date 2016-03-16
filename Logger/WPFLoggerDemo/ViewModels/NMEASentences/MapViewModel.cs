8eeb8ddc7c76fa073c5ace6559f777bc77a00011
AVaden <abvaden801@gmail.com>
2016-03-16 10:12:02 -0400

    Continued work on the WPF demo app got a fix working for the google maps display

--- Logger/WPFLoggerDemo/ViewModels/NMEASentences/MapViewModel.cs
+++ Logger/WPFLoggerDemo/ViewModels/NMEASentences/MapViewModel.cs
@@ -5,29 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 using NMEA_Tools.Serial;
 using NMEA_Tools.Decoder;
+using System.IO;
 
 namespace WPFLoggerDemo.ViewModels.NMEASentences
 {
-    public class MapViewModel : ViewModelBase, IListenerSubscriber
+    public class MapViewModel : ViewModelBase, IListenerSubscriber, IDisposable
     {
-        public string BrowserPage
+        public string BrowserAddress
         {
             get
             {
-                return _GoolgeMaps + _GoogleMapsLocation;
+                /*
+                string key = "AIzaSyC3xLwB_Z7chKbaSvWzqZxljXGa-TGo8IM";
+                string page = string.Format("<iframe src=\"https://www.google.com/maps/embed/v1/view?center={0},{1}&key={2} allowfullscreen />",
+                              Longitude,
+                              Latitude,
+                              key);
+                return page;
+                */
+                string value = "file://" + _LocalPage;
+                return value.Replace("\\","/");
+            }
+            set
+            {
+                _BrowserAddress = value;
             }
         }
         public string Longitude { get; private set; }
         public string Latitude { get; private set; }
+        public string BrowserHeight { get; set; }
+        public string BrowserWidth { get; set; }
 
+        public CefSharp.Wpf.ChromiumWebBrowser Browser { get; set; }
+
+        #region Private Fields
         private Listener _Listener;
 
-        // example google maps link https://www.google.com/maps/place/4916.45,N,12311.12,W
-        private string _GoolgeMaps = @"https://www.google.com/maps/place/";
+        // example google maps link https://www.google.com/maps/place/@4916.45,N,12311.12,W
+        private string _GoolgeMaps = @"https://www.google.com/maps/place/@";
         private string _GoogleMapsLocation;
+        private string _BrowserAddress;
 
         private DateTime _LastUpdate;
         private TimeSpan _MinUpdateRate;
+
+        private string _LocalPage;
+        #endregion
 
         public MapViewModel(Listener listener)
         {
@@ -35,6 +58,7 @@ namespace WPFLoggerDemo.ViewModels.NMEASentences
             _LastUpdate = DateTime.Now;
             _Listener = listener;
             _Listener.Subscribe(this);
+            _LocalPage = Path.GetTempPath() + "GPSMapPage.html";
         }
 
         public void SentenceReceived(string sentence)
@@ -42,26 +66,62 @@ namespace WPFLoggerDemo.ViewModels.NMEASentences
             
             if (sentence.Contains("GPGLL"))
             {
-                NMEA_Tools.Decoder.Sentences.GPGLL gpgll = new NMEA_Tools.Decoder.Sentences.GPGLL(sentence);
+                try
+                {
+                    NMEA_Tools.Decoder.Sentences.GPGLL gpgll = new NMEA_Tools.Decoder.Sentences.GPGLL(sentence);
 
-                _GoogleMapsLocation = String.Format("{0},{1}",
+                    _GoogleMapsLocation = String.Format("{0},{1}",
                     gpgll.Longitude.Value, gpgll.Latitude.Value);
 
-                Longitude = gpgll.Longitude.Value;
-                Latitude = gpgll.Latitude.Value;
+                    Longitude = gpgll.Longitude.Value;
+                    Latitude = gpgll.Latitude.Value;
 
-                if(gpgll.ActiveState.CurrentState == NMEA_Tools.Decoder.Words.ActiveState.ActiveStateEnum.DataActive)
-                {
-                    if((DateTime.Now - _LastUpdate) > _MinUpdateRate)
+                    if ((DateTime.Now - _LastUpdate) > _MinUpdateRate)
                     {
+                        _UpdateLocalPage();
                         _LastUpdate = DateTime.Now;
-                        RaisePropertyChanged("BrowserPage");
+                        RaisePropertyChanged("BrowserAddress");
                         RaisePropertyChanged("Longitude");
                         RaisePropertyChanged("Latitude");
                     }
                 }
+                catch
+                {
+                    Longitude = "No lock yet";
+                    Latitude = "No lock yet";
+                }
+            }
+        }
+
+        private void _UpdateLocalPage()
+        {
+            string html = String.Format(
+                    @"<DOCTYPE html>
+<html>
+<head>
+	<title>Test Page</title>
+</head>
+<body>
+	<iframe src=""https://www.google.com/maps/embed/v1/place?q={0},{1}&key=AIzaSyC3xLwB_Z7chKbaSvWzqZxljXGa-TGo8IM"" />
+</body>
+</html> ",
+                    Latitude.Replace(",", ""),
+                    Longitude.Replace(",", ""));
+
+            File.Delete(_LocalPage);
+            using (FileStream fileStream = System.IO.File.Create(_LocalPage))
+            {
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                {
+                    writer.Write(html);
+                }
             }
 
+        }
+
+        public void Dispose()
+        {
+            System.IO.File.Delete(_LocalPage);
         }
     }
 }
--- Logger/WPFLoggerDemo/Views/NMEASentence/MapView.xaml.cs
+++ Logger/WPFLoggerDemo/Views/NMEASentence/MapView.xaml.cs
@@ -20,29 +20,9 @@ namespace WPFLoggerDemo.Views.NMEASentence
     /// </summary>
     public partial class MapView : UserControl
     {
-        private System.Threading.Timer _Timer;
-
         public MapView()
         {
-
             InitializeComponent();
-            _Timer = new System.Threading.Timer(new System.Threading.TimerCallback(_UpdateMap), null, 3000, 3000);    
-        }
-
-        private void _UpdateMap(object stateInfo)
-        {
-            string key = "AIzaSyC3xLwB_Z7chKbaSvWzqZxljXGa-TGo8IM";
-            string browserWidth = Browser.Width.ToString();
-            string browserHeight = Browser.Height.ToString();
-            string html = string.Format("<iframe width=\"{0}\" height=\"{1}\" frameborder=\"0\" style =\"border:0\" " +
-                          "src=\"https://www.google.com/maps/embed/v1/place?q={2},{3}&key={4}\" allowfullscreen ></iframe >",
-                          browserWidth,
-                          browserHeight,
-                          LongtidueLabel.Content,
-                          LatitudeLabel.Content,
-                          key);
-
-            Browser.NavigateToString(html);
         }
 
     }
